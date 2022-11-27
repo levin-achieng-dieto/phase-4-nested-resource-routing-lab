ee0bd334d719572aea2d97389eaee27d94e76377
levin-achieng-dieto <levin.dieto@student.moringaschool.com>
2022-11-27 08:38:16 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,35 @@
 class ItemsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
-  def index
+def index
+  #check if the request is from nested route or not
+  if params[:user_id]
+    user = User.find(params[:user_id])
+    items = user.items
+  else
     items = Item.all
-    render json: items, include: :user
+  end
+  render json: items, include: :user
+end
+
+def show
+  item = Item.find(params[:id])
+  render json: item, include: :user
+end
+
+def create
+  item = Item.create(create_item)
+  render json: item, status: :created
+end
+
+  private
+
+  def render_not_found_response
+    render json: { error: "Item not found"}, status: :not_found
+  end
+
+  def create_item
+    params.permit(:name, :description, :price, :user_id)
   end
 
 end
